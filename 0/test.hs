d38c5235dc71386cb608a2a834ff2f924fec892a
leventov <leventov@ya.ru>
2012-09-03 19:07:32 +0400

    test

--- 0/test.hs
+++ 0/test.hs
@@ -10,3 +10,24 @@ _abs x = if x >= 0 then x
 sign x | x < 0     = 1
        | x == 0    = 0
        | otherwise = 1
+
+solve a b c = if a == 0 then [simple]
+                        else (if d > 0 then [r1, r2]
+                                       else (if d == 0 then [r1]
+                                                       else error "!"))
+            where
+                simple = -c / b
+                d = b ** 2 - 4 * a * c
+                r1 = (-b + sqrt d) / (2 * a)
+                r2 = (-b - sqrt d) / (2 * a)
+
+
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+
+gcd1 :: Integral a => a -> a -> a
+gcd1 a b | a == 0    = b
+         | b == 0    = a
+         | otherwise = gcd1 b (a rem b)
