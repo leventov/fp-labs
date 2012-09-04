65135845eb55563a66f54ffc8b7b93f933987f20
leventov <leventov@ya.ru>
2012-09-04 15:23:21 +0400

    haskelltop

--- 0/test.hs
+++ 0/test.hs
@@ -1,3 +1,5 @@
+import Data.Bits
+
 s :: Double -> Double
 s r = 4 * pi * r^2
 
@@ -13,8 +15,8 @@ sign x | x < 0     = 1
 
 solve a b c = if a == 0 then [simple]
                         else (if d > 0 then [r1, r2]
-                                       else (if d == 0 then [r1]
-                                                       else error "!"))
+                        else (if d == 0 then [r1]
+                        else error "!"))
             where
                 simple = -c / b
                 d = b ** 2 - 4 * a * c
@@ -47,4 +49,34 @@ mulAccum n m a = mulAccum (n-1) m (a+m)
 mul n m | n < 0 = - mul (-n) m
         | m < 0 = - mul n (-m)
         | otherwise = mulAccum n m 0
+
+
+div2 n = n `shiftR` 1 -- n should be even
+mul2 n = n `shiftL` 1
+
+logMulAccum 0 n a = a
+logMulAccum n 0 a = a
+logMulAccum n m a | odd n     = logMulAccum (n-1) m (a+m)
+                  | otherwise = logMulAccum (div2 n) (mul2 m) a   
+
+logMul n m | n < 0     = - logMul (-n) m
+           | m < 0     = - logMul n (-m)
+           | otherwise = logMulAccum n m 0
+
+
+φ = 2 / (1 + sqrt 5)
+
+localMinIter f a b x1 eps | abs (a - b) < eps = a
+                          | f x2 > f x1       = localMinIter f x2 a x1 eps
+                          | otherwise         = localMinIter f x1 b x2 eps
+                            where x2 = a + (b - a) * φ
+
+localMin f a b eps = localMinIter f a b (b - (b - a) * φ) eps
+
+testFun x = - exp (-x) * log (x)
+
+
+median xs | odd (length xs) = xs !! mid
+          | otherwise     = ((xs !! mid) + (xs !! (mid-1))) / 2 
+            where mid = length xs `div` 2
 
