8e7887bd80d4e747750235d8a0db261b10b8e18c
leventov <leventov@ya.ru>
2012-09-17 18:56:50 +0400

    Stat.hs finished

--- Stat.hs
+++ Stat.hs
@@ -1,24 +1,33 @@
 module Stat () where
 
-import M0 (median)
+import Data.List (sort)
 
+median xs
+    | odd len   = mid
+    | otherwise = realToFrac((mid + (xs !! (midI - 1)))) / 2 
+    where len = length xs
+          midI = len `div` 2
+          mid = xs !! midI
 
-percentile percent xs = (sort xs) !! (length xs * (percent / 100))
+percentile percent xs = realToFrac $ (!!) (sort xs) $ round $ (fromIntegral $ length xs) * (percent / 100)
 
+q1 :: (Real a, Fractional b) => [a] -> b
 q1 = percentile 25
+
+q3 :: (Real a, Fractional b) => [a] -> b
 q3 = percentile 75
 
 iqr xs = q3 xs - q1 xs
 
-lowOutlinerBound xs =
-	q1' - 1.5 * (q3' - q1') 
-	where q1' = q1 xs
-		  q3' = q3 xs
+lowOutlinerBound xs = q1 xs - 1.5 * iqr xs
+highOutlinerBound xs = q3 xs + 1.5 * iqr xs
 
 
-highOutlinerBound xs =
-	q3' + 1.5 * (q3' - q1') 
-	where q1' = q1 xs
-		  q3' = q3 xs
+isOutliner xs x =
+    fx < lowBound || fx > highBound
+    where lowBound = lowOutlinerBound xs
+          highBound = highOutlinerBound xs
+          fx = realToFrac x
 
-minimumIgnoreOutliners xs = 
+regular xs = filter (not . isOutliner xs) xs
+outliners xs = filter (isOutliner xs) xs
