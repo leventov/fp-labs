78e2b0194139b461886e96fa2a404d6ae75b0058
leventov <leventov@ya.ru>
2012-10-02 15:21:41 +0400

    Words stemming - initial

--- Words.hs
+++ Words.hs
@@ -1,6 +1,23 @@
-import Data.Map
+import Data.Map (alter, empty, assocs)
+import Data.Char (isPunctuation, toLower)
+import System.Environment (getArgs)
+import GHC.Exts (sortWith)
+
+perfectiveGerund rw = 
+
+stem :: String -> String
+stem = id
 
 count Nothing  = Just 1
 count (Just f) = Just $ f + 1
 
 frequencyList = foldl (flip $ alter count) empty
+
+main = do
+    (fn:_) <- getArgs
+    content <- readFile fn
+    let adoptedContent = map toLower . filter (not . isPunctuation) $ content
+        adoptedWords = map stem $ words adoptedContent
+        fql = sortWith snd . assocs . frequencyList $ adoptedWords
+        pprint (a, b) = a ++ " -> " ++ show b ++ "\n"
+    putStr . concat . map pprint $ fql
