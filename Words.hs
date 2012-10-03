d5dcd9a214cf5d3b35d9bf4e82ad4b1edf61e077
leventov <leventov@ya.ru>
2012-10-03 20:20:11 +0400

    Words stemming completed

--- Words.hs
+++ Words.hs
@@ -11,12 +11,13 @@ import Data.Map (alter, empty, assocs)
 import System.Environment (getArgs)
 
 import GHC.Exts (sortWith)
-import Text.Printf (printf)
 
+-- stemming: exact implementation of
+-- http://snowball.tartarus.org/algorithms/russian/stemmer.html
 isVowel c = c `elem` "аеиоуыэюя"
 
 data StemWord = StemWord {reversedString :: String,
-                          rvBound :: Int, r2Bound :: Int}
+                          rvBound :: Int, r2Bound :: Int} deriving (Show)
 
 toStemWord s =
     let boundProcess p rs =
@@ -36,27 +37,28 @@ toStemWord s =
 
 fromStemWord (StemWord s _ _) = reverse s
 
-
+-- "Right" means fail with current remove rule, to continue with the next.
+-- "Left" means success with removing, => pass the rest of rules. 
 type RemoveTry = Either StemWord StemWord
 
 unwrap :: RemoveTry -> StemWord
 unwrap = either id id
 
+continueAnyway :: RemoveTry -> RemoveTry
 continueAnyway tr = Right $ unwrap tr
 
+-- v. v. of default RemoveTry semantics
+continueIfRemoved :: RemoveTry -> RemoveTry
 continueIfRemoved (Left sw) = Right sw
 continueIfRemoved (Right sw) = Left sw
 
 
-type SuffixBound = (StemWord -> Int)
+type SelectBound = (StemWord -> Int)
 
 data Endings = Endings {endings :: [String], preceded :: [Char],
-                        bound :: SuffixBound}
+                        bound :: SelectBound}
 
-instance Show Endings where
-    show (Endings es p _) = printf "Endings [%s %s someFun]" (show es) (show p)
-
-
+-- kind :: [Endings]
 perfectiveGerund = [
     Endings ["ив", "ивши", "ившись", "ыв", "ывши", "ывшись"] "" rvBound,
     Endings ["в", "вши", "вшись"] "ая" rvBound]
@@ -83,72 +85,68 @@ noun = [Endings ["а", "ев", "ов", "ие", "ье", "е", "иями", "ями
     "ья", "я"] "" rvBound]
 
 superlative = [Endings ["ейш", "ейше"] "" rvBound]
-
 derivational = [Endings ["ост", "ость"] "" r2Bound]
-
 i = [Endings ["и"] "" rvBound]
-
 softSign = [Endings ["ь"] "" rvBound]
-
 doubleN = [Endings ["н"] "н" rvBound]
+-- / kinds
 
 tryRemove :: [Endings] -> StemWord -> RemoveTry
 tryRemove es = foldl1 (>=>) $ map tryRemove' es
 
 tryRemove' :: Endings -> StemWord -> RemoveTry
-tryRemove' (Endings es ps bound) sWord = foldM tryRemoveEnding sWord rEndings
+tryRemove' (Endings es ps selectBound) sWord =
+    foldM tryRemoveEnding sWord rEndings
     where
         rEndings = map reverse es
-        withP = (length ps) == 0
-        endingL e = length (e) + (if withP then 0 else 1)
-        
-        tryRemoveEnding sw@(StemWord rString rv r2) rEnding = 
-            let el = endingL rEnding
+        withP = length ps /= 0
+        endingBoundL e = length e + if withP then 0 else 1
+
+        tryRemoveEnding sw@(StemWord rString rv r2) rEnding =
+
+            if endingInBound && endingIsSuffix && properlyPreceded
+                then Left reminder
+                else Right sw
+            where
+                ebl = endingBoundL rEnding
+                endingInBound = ebl <= selectBound sw
+
                 endingIsSuffix = isPrefixOf rEnding rString
-                endingInBound = el <= bound sw
-                properlyPreceded = not withP || rString !! (el - 1) `elem` ps
+
+                el = length rEnding
+                properlyPreceded = not withP || rString !! el `elem` ps
+                
                 reminder = StemWord (drop el rString) (rv - el) (r2 - el)
-            in if endingInBound && endingIsSuffix && properlyPreceded
-                    then Left reminder
-                    else Right sw
          
 
-
 tryRemoveAdjectival sw = do
-    sw1 <- continueAnyway $ tryRemove adjective sw
-    tryRemove participle sw1
+    s1 <- continueAnyway $ tryRemove adjective sw
+    tryRemove participle s1
 
+-- step* :: StemWord -> RemoveTry
+step1 = tryRemove perfectiveGerund >=> way2
+    where way2 sw = do
+            s1 <- continueAnyway $ tryRemove reflexive sw
+            tryRemoveAdjectival >=> tryRemove verb >=> tryRemove noun $ s1
 
-step1 :: StemWord -> RemoveTry
-step1 sw = do
-    sw2 <- tryRemove perfectiveGerund sw
-    let way2 sw3 = do
-            sw4 <- continueAnyway $ tryRemove reflexive sw3
-            sw5 <- tryRemoveAdjectival sw4
-            sw6 <- tryRemove verb sw5
-            tryRemove noun sw6
-    way2 sw2
-
-step2 :: StemWord -> RemoveTry
 step2 = tryRemove i
 
-step3 :: StemWord -> RemoveTry
 step3 = tryRemove derivational
 
-step4 :: StemWord -> RemoveTry
-step4 sw = do
-    sw2 <- tryRemove doubleN sw
-    let way2 sw3 = do
-        sw4 <- continueIfRemoved $ tryRemove superlative sw3
-        tryRemove doubleN sw4
-    sw5 <- way2 sw2
-    tryRemove softSign sw5
+step4 = tryRemove doubleN >=> way2 >=> tryRemove softSign
+    where way2 sw = do
+            s1 <- continueIfRemoved $ tryRemove superlative sw
+            tryRemove doubleN s1
 
 
 stem :: String -> String
 stem = fromStemWord . steps . toStemWord
-    where steps = foldr (.) id $ map (unwrap .) [step4, step3, step2, step1]
+    where steps = foldr1 (.) $ map (unwrap .) [step4, step3, step2, step1]
+    -- the TRUE way would be
+    -- steps = unwrap $ foldl1 (>=>)
+    --                $ map (continueAnyway .) [step1, step2, step3, step4]
 
+-- / stemming
 
 count Nothing  = Just 1
 count (Just f) = Just $ f + 1
@@ -163,3 +161,5 @@ main = do
         fql = sortWith snd . assocs . frequencyList $ adoptedWords
         pprint (a, b) = a ++ " -> " ++ show b ++ "\n"
     putStr . concat . map pprint $ fql
+
+-- P. S. this program processes War and Peace text in ~2 minutes
