73489f751e05d47181d0225502055f81be04eb88
leventov <leventov@ya.ru>
2012-10-17 15:27:12 +0400

    errors in CSV parser. XML parser completed

--- CSV.hs
+++ CSV.hs
@@ -2,6 +2,7 @@ import Control.Applicative ((<*), (<$))
 
 import Text.ParserCombinators.Parsec
 
+-- according to http://en.wikipedia.org/wiki/Comma-separated_values
 
 eol = try (string "\n\r") <|>
       try (string "\r\n") <|>
@@ -12,9 +13,8 @@ quotes = between (char '"') (char '"')
 
 eols = "\r\n"
 
-quotedValue delim = quotes $ many $ doubleQuote <|> nonQuoteAcceptable
+quotedValue delim = quotes $ many $ doubleQuote <|> noneOf "\""
     where doubleQuote = try $ '"' <$ string "\"\""
-          nonQuoteAcceptable = noneOf $ '"':delim:eols
 
 value delim = quotedValue delim <|> many acceptable
     where acceptable = noneOf $ delim:eols
@@ -22,24 +22,24 @@ value delim = quotedValue delim <|> many acceptable
 line delim = sepBy (value delim) (char delim)
 
 csv delim False = endBy (line delim) eol
-csv delim True =
-    [] <$ eof <|>
-    do firstRow <- (line delim) <* eol
-       let colCount = length firstRow
-           val = value delim
-           row = do
-               firstValue <- val
-               restValues <- sequence $ replicate (colCount - 1) $
-                   char delim >> val
-               return $ firstValue:restValues
-               
-       restRows <- endBy row eol
-       -- without this csv consumes only 1st line of non-table input.
-       eof -- leads to error
-       return $ firstRow:restRows
+csv delim True = [] <$ eof <|> do
+    firstRow <- (line delim) <* eol
+    let colCount = length firstRow
+        val = value delim
+        row = do
+            firstValue <- val
+            restValues <- count (colCount - 1) $ char delim >> val
+            return $ firstValue:restValues
+
+    restRows <- endBy row eol
+    -- without this csv consumes only 1st line of non-table input.
+    eof -- leads to error
+    return $ firstRow:restRows
 
 
 parseCSV :: Char -> Bool -> String -> Either ParseError [[String]]
-parseCSV delim table = parse (csv delim table) ""
+parseCSV delim tableMode = parse (csv delim tableMode) ""
 
 simpleCSV = parseCSV ',' False
+
+main = putStrLn . either show show . simpleCSV =<< getContents
--- XML.hs
+++ XML.hs
@@ -1,19 +1,45 @@
 import Control.Applicative ((<*), (<$), (<$>))
+import Control.Monad ((<=<))
 
-import Text.ParserCombinators.Parsec hiding (Parser)
+import Data.List (intercalate)
+import Data.Char (isSpace)
 import Numeric (readHex, readDec)
-import Data.Maybe (fromMaybe)
+import Data.String.Utils (strip)
+import Text.ParserCombinators.Parsec hiding (Parser)
 
 type Parser t = CharParser () t
 
---quotes = between (char '"') (char '"')
+-- Simplified XML parser
+-- Mainly according to http://www.w3.org/TR/2006/REC-xml11-20060816/
+-- Without declarations and CDATA
+-- With empty tags, comments, entities and hexidecimal char replacements,
+-- commentless and spaceless modes
 
-type Attr = (String, String)
-data Node = Tag {getName :: String, getAttrs :: [Attr], getChildren :: [Node]} |
-            Content {getContent :: String} |
-            Comment
+enquote c s = [c] ++ s ++ [c]
 
-type Tree = Node
+newtype Attr = Attr {getAttr :: (String, String)}
+instance Show Attr where
+    show (Attr (n, v))
+        | '"' `elem` v = n ++ "=" ++ enquote '\'' v
+        | otherwise    = n ++ "=" ++ enquote '"' v
+
+data Node =
+    Tag {getName :: String, getAttrs :: [Attr], getChildren :: [Node]} |
+    Content {getContent :: String} |
+    Comment {getComment :: String}
+
+indented :: Int -> Node -> String
+indented i node = (replicate i '\t') ++ case node of
+    (Tag n as ch) -> let desc = intercalate " " (n:(map show as))
+                     in intercalate "\n" (desc:(map (indented (i + 1)) ch))
+    (Content c)   -> c
+    (Comment c)   -> "Comment " ++ enquote '"' c
+
+instance Show Node where
+    show = indented 0
+
+-- A Tag. Not content or comment! May turn to "tag with declarations, comments"
+type Document = Node
 
 charRef :: Parser Char
 charRef = do
@@ -24,57 +50,92 @@ charRef = do
             let ((d, _):_) = rf digits
             return $ toEnum d
 
+
 reference p = char '&' >> p <* char ';'
+entities = [('\'', "apos"), ('"', "quot"), ('<', "lt"), ('>', "gt"), ('&', "amp")]
 
-replacements = [('\'', "apos"), ('"', "quot"), ('<', "lt"), ('>', "gt")]
-replace :: String -> Parser Char
-replace cs = noneOf cs <|>
-             choice $ map (reference . string) repls
-    where repls = fromMaybe [] .sequence . map (`lookup` replacements) $ cs
+xmlReplace :: String -> Parser Char
+xmlReplace cs = reference $ (<|>) charRef $ choice $ flip map cs $ \c -> do
+    let repl = lookup c entities
+    maybe (fail "No such named entity") ((<$) c . try . string) repl
+
+replaced :: String -> Parser Char
+replaced rs = noneOf xmlRs <|> xmlReplace xmlRs
+    where xmlRs = '<':'&':rs
 
 
-tagName :: Parser String
-tagName = undefined
+anyName :: Parser String
+anyName = do
+    let startChar = letter <|> oneOf ":_"
+    sChar <- startChar
+    restChars <- many $ startChar <|> digit <|> oneOf "-."
+    return $ sChar:restChars
 
-attr :: Parser Attr
-attr = undefined
-
-attrName :: Parser String
-attrName = undefined
+tagName = anyName
+attrName = anyName
 
 attrValue :: Parser String
-attrValue = undefined
+attrValue = quoted '"' <|> quoted '\''
+    where quoted q = between (char q) (char q) $ many $ replaced [q]
 
 content :: Parser String
-content = many $ replace "<&"
+content = many1 $ replaced ""
+
+
+attr :: Parser Attr
+attr = do
+    n <- attrName
+    spaces >> char '=' >> spaces
+    v <- attrValue
+    return $ Attr (n, v)
+
 
 comment :: Parser Node
-comment = do string "<!--"
-             many $ noneOf "-" <|> (anyChar >> noneOf "-")
-             string "-->"
-             return Comment
+comment = do
+    string "<!--"
+    c <- many $ noneOf "-" <|> (try $ anyChar >> noneOf "-")
+    string "-->"
+    return $ Comment c
 
 tag :: Parser Node
 tag = do
-    char '<'
-    name <- tagName
-    spaces
+    name <- char '<' >> tagName <* spaces
     attrs <- attr `endBy` spaces
+    let endTag = string "</" >> string name >> spaces >> char '>'
 
-    let endTag = char '<' >> string name >> spaces >> string "/>"
-    allChildren <- [] <$ string "/>" <|> many node <* endTag
-    
-    let notComment Comment = False
-        notComment _       = True
-        children = filter notComment allChildren
-    
+    children <-
+        [] <$ string "/>" <|> -- empty tag
+        (char '>' >> manyTill node (try endTag))
+
     return $ Tag name attrs children
 
-
 node :: Parser Node
-node = try comment <|> tag <|> Content <$> content
+node = try comment <|> try tag <|> Content <$> content
 
 
-parseXML :: Parser Tree
-parseXML = undefined
+parseXML :: String -> Either ParseError Document
+parseXML = parse (spaces >> tag <* spaces <* eof) "(unknown)"
 
+
+spaceless :: Node -> Node
+spaceless (Tag n as ch) =
+    let emptyNode (Content c) = all isSpace c
+        emptyNode _           = False
+    in Tag n as $ map spaceless . filter (not . emptyNode) $ ch
+spaceless (Content c)   = Content $ strip c
+spaceless (Comment c)   = Comment $ strip c
+
+
+commentless :: Document -> Document
+commentless (Tag n as ch) = Tag n as $ map applyCh $ filter notC ch
+    where notC (Comment _) = False
+          notC _           = True
+          applyCh c@(Content _) = c
+          applyCh t@(Tag _ _ _) = commentless t
+-- Document should NOT be content or comment
+
+
+main = do
+    xmlS <- getContents
+    let unwrap = either show show
+    putStrLn . unwrap $ return . commentless . spaceless =<< parseXML xmlS
